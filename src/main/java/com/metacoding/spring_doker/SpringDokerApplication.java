4a59ef0908f2794226fcbfe966e1e98b9f95ef98
kjh5848 <kjh5848@gmail.com>
2025-10-30 17:42:38 +0900

    first commit

---
+++ src/main/java/com/metacoding/spring_doker/SpringDokerApplication.java
@@ -0,0 +1,13 @@
+package com.metacoding.spring_doker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringDokerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringDokerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/metacoding/spring_doker/SpringDokerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.metacoding.spring_doker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringDokerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
